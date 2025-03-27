64e4566264cf9debe46144480351c15143768182
Sam <156055395+TehhX@users.noreply.github.com>
2025-03-26 23:43:57 -0400

    Changed EmptyWindow/src/main.cpp to define and declare WindowProc in same place.

--- EmptyWindow/src/main.cpp
+++ EmptyWindow/src/main.cpp
@@ -1,6 +1,23 @@
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT:
+        PAINTSTRUCT ps;
+        // All painting occurs here, between BeginPaint and EndPaint.
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW - 2));
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
     // Register the window class.
@@ -15,7 +32,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     // Create the window.
     HWND hwnd = CreateWindowEx(
-        0,                           // Optional window styles.
+        1,                           // Optional window styles.
         CLASS_NAME,                  // Window class
         L"Learn to Program Windows", // Window text
         WS_OVERLAPPEDWINDOW,         // Window style
@@ -43,23 +60,4 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     }
 
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-    case WM_PAINT:
-        PAINTSTRUCT ps;
-        // All painting occurs here, between BeginPaint and EndPaint.
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-        EndPaint(hwnd, &ps);
-        return 0;
-    }
-
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
